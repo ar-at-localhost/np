86605dbc4307849f5efc2225dcc4e552df3db052
Ahmad Raza <pro.se.ahmad.raza.1@gmail.com>
2026-03-09 19:09:42 +0500

    fix: session behaviors

--- modules/neovim/plugins/auto-session.nix
+++ modules/neovim/plugins/auto-session.nix
@@ -1,24 +1,11 @@
 {
   plugins.auto-session.settings = {
-    root_dir.__raw = ''
-      (function()
-        -- detect project via .git or flake.nix
-        local cwd = vim.loop.cwd()
-
-        local found = vim.fs.find({ ".git", "flake.nix" }, {
-          upward = true,
-          path = cwd,
-          stop = vim.loop.os_homedir(),
-        })[1]
-
-        if found and found ~= "" then
-          -- storage: <project>/.nvim/sessions
-          return vim.fs.dirname(found) .. "/.nvim/sessions"
-        end
-
-        -- fallback: stdpath("data")/sessions
-        return vim.fn.stdpath("data") .. "/sessions"
-      end)()
-    '';
+    auto_create = true;
+    auto_save = true;
+    auto_restore = true;
+    single_session_mode = true;
+    close_filetypes_on_save = ["checkhealth" "neo-tree" "snacks_terminal"];
+    root_dir.__raw = "_M.root .. '.nvim/sessions'";
+    post_restore_cmds = ["Neotree show"];
   };
 }
