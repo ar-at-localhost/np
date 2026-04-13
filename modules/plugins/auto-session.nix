b813abb9a129616a2f56f3459dc8de07cc740699
Ahmad Raza <pro.se.ahmad.raza.1@gmail.com>
2026-04-13 14:53:22 +0500

    fix: auto create session

--- modules/plugins/auto-session.nix
+++ modules/plugins/auto-session.nix
@@ -8,4 +8,10 @@
     root_dir.__raw = "_M.dirs.sessions";
     post_restore_cmds = ["Neotree show"];
   };
+
+  plugins.auto-session.luaConfig.post = ''
+    if _M.dirs and _M.dirs.sessions then
+      require("auto-session").auto_save_session()
+    end
+  '';
 }
